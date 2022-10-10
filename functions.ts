b6a7b97ef13b25530aa3c8069a34850a0b99f03e
Marek Kobida <marek.kobida@gmail.com>
2022-10-10 23:55:59 +0200

    functions.ts updated

--- functions.ts
+++ functions.ts
@@ -9,3 +9,19 @@ export function isLeft<E>($: Either<E, unknown>): $ is Left<E> {
 export function isRight<T>($: Either<unknown, T>): $ is Right<T> {
   return $.$ === 'Right';
 }
+
+// ✅
+export function left<E, T = never>(e: E): Either<E, T> {
+  return {
+    $: 'Left',
+    left: e,
+  };
+}
+
+// ✅
+export function right<T, E = never>(t: T): Either<E, T> {
+  return {
+    $: 'Right',
+    right: t,
+  };
+}
