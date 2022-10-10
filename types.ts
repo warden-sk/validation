ce539c9a264f204d2e11ce03e4b5e5ac64ac6009
Marek Kobida <marek.kobida@gmail.com>
2022-10-11 01:14:14 +0200

    types.ts

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import type { Type } from '.';
+
 // ✅
 export interface Left<E> {
   readonly $: 'Left';
@@ -17,3 +19,22 @@ export type Either<E, T> = Left<E> | Right<T>;
 
 // ✅
 export type Is<T> = (input: unknown) => input is T;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// ✅
+export type Validate<Input, T> = (input: Input, context: ValidationContext[]) => Validation<T>;
+
+// ✅
+export type Validation<T> = Either<ValidationError[], T>;
+
+export interface ValidationContext {
+  readonly input: unknown;
+  readonly key: string;
+  readonly type: Type<any, any>;
+}
+
+export interface ValidationError {
+  readonly context: ValidationContext[];
+  readonly value: unknown;
+}
