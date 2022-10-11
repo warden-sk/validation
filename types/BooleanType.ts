a064d82bbf1bcfec14d1ed662ea81c42873d37f2
Marek Kobida <marek.kobida@gmail.com>
2022-10-11 18:54:13 +0200

    BooleanType.ts

---
+++ types/BooleanType.ts
@@ -0,0 +1,13 @@
+import Type from '../Type';
+
+class BooleanType extends Type<boolean> {
+  constructor() {
+    super(
+      'boolean',
+      (input): input is boolean => typeof input === 'boolean',
+      (input, context) => (this.is(input) ? r(input) : l(input, context))
+    );
+  }
+}
+
+export default BooleanType;
