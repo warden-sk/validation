26c241d61501aad6af175af55d1fdb11a155786d
Marek Kobida <marek.kobida@gmail.com>
2022-10-11 20:16:07 +0200

    UndefinedType.ts

---
+++ types/UndefinedType.ts
@@ -0,0 +1,13 @@
+import Type from '../Type';
+
+class UndefinedType extends Type<undefined> {
+  constructor() {
+    super(
+      'undefined',
+      (input): input is undefined => typeof input === 'undefined',
+      (input, context) => (this.is(input) ? r(input) : l(input, context))
+    );
+  }
+}
+
+export default UndefinedType;
