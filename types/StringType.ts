b45b624d040b8dfcfead5211a1f604241cd7dfe3
Marek Kobida <marek.kobida@gmail.com>
2022-10-14 13:42:32 +0200

    update

--- types/StringType.ts
+++ types/StringType.ts
@@ -9,7 +9,7 @@ class StringType extends Type<string> {
     super(
       'string',
       (input): input is string => typeof input === 'string',
-      (input, context) => (this.is(input) ? r(input) : l(input, context))
+      (input, context) => (this.is(input) ? this.right(input) : this.left(input, context))
     );
   }
 }
