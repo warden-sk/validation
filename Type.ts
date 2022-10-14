ac4b9bc8402243fdf3a5a667a24fd23618ad93e6
Marek Kobida <marek.kobida@gmail.com>
2022-10-14 13:43:04 +0200

    update

--- Type.ts
+++ Type.ts
@@ -1,10 +1,19 @@
-import type { Decoder, Is, Validate, Validation } from './types';
+import type { Decoder, Is, Validate, Validation, ValidationContext } from './types';
+import { left, right } from './functions';
 
 class Type<T, I = unknown> implements Decoder<I, T> {
   constructor(readonly name: string, readonly is: Is<T>, readonly validate: Validate<I, T>) {}
 
   decode(i: I): Validation<T> {
     return this.validate(i, [{ input: i, key: '', type: this }]);
+  }
+
+  left<T>(value: unknown, context: ValidationContext[]): Validation<T> {
+    return left([{ context, value }]);
+  }
+
+  right<T>(value: T): Validation<T> {
+    return right(value);
   }
 }
 
