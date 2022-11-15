2e2bded2accff301b5c9eb6866e5b6c2be11919c
Marek Kobida <marek.kobida@gmail.com>
2022-11-15 21:39:46 +0100

    errorMessages

---
+++ helpers/errorMessages.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+ * Copyright 2022 Marek Kobida
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const errorMessages = {
+    FILE_NOT_READABLE: 'The file is not readable.',
+    FILE_NOT_WRITABLE: 'The file is not writable.',
+    FUNCTION_NOT_IMPLEMENTED: 'The function is not implemented.',
+    JSON_NOT_DECODABLE: 'The file is not decodable.',
+    JSON_NOT_ENCODABLE: 'The file is not encodable.',
+};
+exports.default = errorMessages;
---
+++ helpers/errorMessages.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2022 Marek Kobida
+ */
+
+const errorMessages = {
+  FILE_NOT_READABLE: 'The file is not readable.',
+  FILE_NOT_WRITABLE: 'The file is not writable.',
+  FUNCTION_NOT_IMPLEMENTED: 'The function is not implemented.',
+  JSON_NOT_DECODABLE: 'The file is not decodable.',
+  JSON_NOT_ENCODABLE: 'The file is not encodable.',
+} as const;
+
+export default errorMessages;
